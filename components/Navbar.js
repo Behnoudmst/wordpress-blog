be9f4abc65643e12da0d9d98c92d69ae6428cb05
ben mst <mostafaie@behnoud.net>
2023-04-16 16:22:56 +0200

    fix link color in nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
 
     return (
       <>
-          <nav className="bg-black md:flex flex-wrap items-center justify-items-stretch text-white justify-between md:px-14 basis1/3 gap-2 p-3">
+          <nav className="bg-slate-800 md:flex flex-wrap items-center justify-items-stretch text-white justify-between md:px-14 basis1/3 gap-2 p-3">
               <Link href="/" passHref ><h1 className="text-center cursor-pointer">Behnoud Mostafaie</h1></Link>
               <div className="flex gap-6 justify-around text-center">
-                  <Link href="/">Home</Link>  
-                  {session ? <><p className="italic">{session.user.name}</p><span onClick={signOut} className="cursor-pointer">Sign out</span> </> : <a className="cursor-pointer" onClick={signIn}>Sign in</a> }     
+                  <Link  href="/" passHref ><p className="text-white cursor-pointer">Home</p></Link>  
+                  {session ? <><p className="italic">{session.user.name}</p><span onClick={signOut} className="cursor-pointer text-white">Sign out</span> </> : <span className="cursor-pointer text-white" onClick={signIn}>Sign in</span> }     
               </div>
           </nav>
       </>
