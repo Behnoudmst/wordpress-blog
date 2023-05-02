3b8700d65c8a8a6310e078d327c934152c869120
ben mst <mostafaie@behnoud.net>
2023-05-02 18:55:43 +0200

    made header navbar text smaller

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,9 +7,9 @@ export default function Navbar() {
 
     return (
       <>
-          <nav className="bg-slate-800 md:flex flex-wrap items-center justify-items-stretch text-white justify-between md:px-14 basis1/3 gap-2 p-3">
-              <Link href="/" passHref ><h1 className="text-center cursor-pointer">Behnoud Mostafaie</h1></Link>
-              <div className="flex gap-6 justify-around text-center">
+          <nav className="bg-black md:flex flex-wrap items-center justify-items-stretch text-white justify-between md:px-14 basis1/3 gap-2 p-3">
+              <Link href="/" passHref ><h1 className=" cursor-pointer text-lg">Behnoud Mostafaie</h1></Link>
+              <div className="flex gap-6 justify-around text-right">
                   <Link  href="/" passHref ><p className="text-white cursor-pointer">Home</p></Link>  
                   {session ? <><p className="italic">{session.user.name}</p><span onClick={signOut} className="cursor-pointer text-white">Sign out</span> </> : <span className="cursor-pointer text-white" onClick={signIn}>Sign in</span> }     
               </div>
